3f3a35af08eca973092396793086367501843bb8
OlivetEkeh <ekehsons@gmail.com>
2024-03-24 22:41:43 +0100

    task3

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Define SSH client configuration
+# include stdlib
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}-> file_line { 'Use private key in ~/.ssh/school':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}-> file_line { 'Disable password Authentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
